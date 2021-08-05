1920ce8f48966081cd913dcd5e2cbea1919c514a
Gead Kee <gangeadkee@gmail.com>
2021-08-06 00:27:44 +0800

    update index.php flex styles
    la

--- PiltonHolten/index.php
+++ PiltonHolten/index.php
@@ -13,10 +13,11 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" type="text/css" href="merchpage.css" >
     <title>Welcome to Pilton Hotel</title>
 </head>
+<body>
     <main>
         
 
@@ -40,15 +41,15 @@ session_start();
                 while($roomdisplay = mysqli_fetch_array($result)){
                 ?>            
                 <div class="card">
-                    <!-- <div class="card-img"> -->
+                    <div class="card-img">
                         <?php echo '<img src="data:image;base64,'. base64_encode($roomdisplay['room_pic']) .'" alt="room pic" style="width:100%" >'; ?>
                         <img src="" alt="" style="width:100%">
-                    <!-- </div> -->
-                    <!-- <div class="card-info"> -->
+                    </div>
+                    <div class="card-info">
                         <h1><?php echo $roomdisplay['room_type']; ?></h1>
                         <p class="price">RM <?php echo $roomdisplay['price']; ?></p>
                         <p><?php echo $roomdisplay['room_desc']; ?></p>
-                    <!-- </div> -->
+                    </div>
                     <button><a href="login.php" style="text-decoration: none; color: #e0fff4;">Book Now</a></button>
                 </div>    
                 </section>
